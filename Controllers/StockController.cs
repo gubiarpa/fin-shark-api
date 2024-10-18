f6bf317b8ea89cbf1f5a9b5341d3a676f977b581
Billy Arredondo <billyarredondo.coder@gmail.com>
2024-10-18 04:58:30 -0500

    Stock Controller

---
+++ Controllers/StockController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using net8_training.Data;
+
+namespace net8_training.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StockController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var stocks = await _context.Stocks.ToListAsync();
+
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            var stock = await _context.Stocks.FindAsync(id);
+
+            if (stock == null)
+                return NotFound();
+
+            return Ok(stock);
+        }
+    }
+}
