2373812573357a660279692d97dbd58c18f82613
Billy Arredondo <billyarredondo.coder@gmail.com>
2024-10-18 03:39:25 -0500

    DB Context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using net8_training.Models;
+
+namespace net8_training.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Stock> Stocks { get; set; } = null!;
+        public DbSet<Comment> Comments { get; set; } = null!;
+    }
+}
